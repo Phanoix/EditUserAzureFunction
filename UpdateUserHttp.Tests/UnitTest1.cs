93c51a285592bbea6daf2628a52b90522f9b28be
Ilia <phanoix@gmail.com>
2021-03-10 10:52:43 -0500

    Update UnitTest1.cs

--- UpdateUserHttp.Tests/UnitTest1.cs
+++ UpdateUserHttp.Tests/UnitTest1.cs
@@ -4,7 +4,7 @@ using System.Text;
 using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Azure.WebJobs.Host;
-using UpdateUserHttp
+using UpdateUserHttp;
 
 namespace UpdateUserHttp.Tests
 {
